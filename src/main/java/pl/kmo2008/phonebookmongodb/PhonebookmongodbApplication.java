109abee4803d4ab2b035ce54279665063c281a3d
Kmo2008 <kamilostry20@gmail.com>
2018-03-21 10:43:07 +0100

    Initial commit

---
+++ src/main/java/pl/kmo2008/phonebookmongodb/PhonebookmongodbApplication.java
@@ -0,0 +1,12 @@
+package pl.kmo2008.phonebookmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebookmongodbApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/kmo2008/phonebookmongodb/PhonebookmongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.kmo2008.phonebookmongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhonebookmongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
